e8f54084b8749dd655d3023ee61b3cb0690e732a
Unname-addictioner <68629080+Unname-addictioner@users.noreply.github.com>
2020-10-23 17:00:51 +0900

    k

--- sample/js/script03.js
+++ sample/js/script03.js
@@ -1,5 +1,23 @@
 $(function(){
 
+let hako = 0;
+$("#button01").on("click",function(){
+    for(let i=0;i , 10; i++){
+        hako = hako + 2;
+    }
+    $("#content").text(hako);
+})
 
-  
+$("#button03").on("click",function(){
+    setInterval(function(){
+        hako = hako + 1;
+        $("#content").text(hako);
+    },1000)
+})
+
+$("#button04").on("click",function(){
+    hako = 0;
+    $("#content").text(hako);
+})
+
 })
