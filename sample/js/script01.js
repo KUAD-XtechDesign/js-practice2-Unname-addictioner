f929b2e3b7d124d336925606735c232f463b3143
Unname-addictioner <68629080+Unname-addictioner@users.noreply.github.com>
2020-10-23 15:26:02 +0900

    d

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,5 +1,43 @@
 $(function(){
+   
+    let txt1 = "おはようございます！";
+    let txt2 = "お元気ですか？";
+    let num1 = 20;
+    let num2 = 32;
 
+    function sayHello(){
+        $("#content").text("こんにちわ！");
+    }
+
+    function saySomething(txt){
+        $("#content").text(txt);
+    }
+
+    function samTwo(t1,t2){
+        $("#content").text(t1 + t2);
+    }
+
+     $("#button01").on("click",function(){
+            sayHello();
+        })
+    
+        $("#button02").on("click",function(){
+            saySomething(txt1);
+        })
+    
+        $("#button03").on("click",function(){
+           samTwo(txt1,txt2);
+        })
+    
+        $("#button04").on("click",function(){
+            samTwo(num1,num2);
+        })
+        
+        $("#button05").on("click",function(){
+           samTwo(txt1,txt2);
+           samTwo(num1,num2);
+        })
+    
 
 
 
